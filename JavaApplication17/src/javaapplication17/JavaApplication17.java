7b5a68db6b91b7814af5a2693fe2636499caffe3
amadeo <amadeo@localhost>
2020-10-02 12:26:26 +0200

    version 0 de Hola mundo

--- JavaApplication17/src/javaapplication17/JavaApplication17.java
+++ JavaApplication17/src/javaapplication17/JavaApplication17.java
@@ -16,7 +16,7 @@ public class JavaApplication17 {
      */
     public static void main(String[] args)
     {
-        System.out.println("Hola Mundos");
+        System.out.println("Hola Mundosiiiiiii");
     }
     
 }
